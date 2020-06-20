c7ae69cc3bda32c8f205a7dbe7e8aea0edeb64de
a-givertzman <a.givertzman@icloud.com>
2020-06-20 21:56:10 +0300

    project structure

---
+++ lib/core/error/failures.dart
@@ -0,0 +1,9 @@
+import 'package:equatable/equatable.dart';
+
+abstract class Failure extends Equatable {
+  Failure([List properties = const <dynamic>[]]) : super();
+
+  // TODO: implement props
+  List<Object> get properties => <dynamic>[];
+
+}
